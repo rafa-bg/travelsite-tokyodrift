0b2a63caaf05dd84eff8a924ea5c579063c0f0a7
Rafael <rafaelbaez.diseno@gmail.com>
2019-04-17 14:43:40 -0500

    reordene los tasks y puse el html y css a como estan antes

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+gulp.task('watch', function() {
+	//inicia browsersync
+	browserSync.init({
+		//desactiva las notificaciones
+		notify: false,
+		//indica el directorio del cual se va a servir es servidor local
+		server: {
+			baseDir: "app"
+		}
+	});
+	//inicia browsersync
+
+	//wacha el archivo index.html
+	watch('./app/index.html', function() {
+		//cuando observe un cambio, recarga la pagina con browser sync
+		browserSync.reload();
+	});
+	//wacha en la carpeta styles /*subcarpetas*/*uncss.css, cuando haya un cambio, inicia cssInject
+	watch('./app/assets/styles/**/*.css', function() {
+		gulp.start('cssInject');
+	});
+});
+//cssInject no sucede hasta que la task "styles" haya sido completada con exito
+gulp.task('cssInject', ['styles'],function() {
+	return gulp.src('./app/temp/styles/styles.css')
+		.pipe(browserSync.stream());
+});
