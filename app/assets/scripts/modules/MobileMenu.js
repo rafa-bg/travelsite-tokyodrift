b3a6343af02d063159fc3343fadc0991c174c313
Rafael <rafaelbaez.diseno@gmail.com>
2019-04-19 22:34:46 -0500

    Menu responsive y efecto del icono

---
+++ app/assets/scripts/modules/MobileMenu.js
@@ -0,0 +1,29 @@
+import $ from 'jquery';
+
+class MobileMenu {
+	constructor() {
+		this.siteHeader = $(".site-header");
+		this.menuIcon = $(".site-header__menu-icon");
+		this.menuContent = $(".site-header__menu-content");
+		this.events()
+	}
+
+	events() {
+		this.menuIcon.click(this.toggleTheMenu.bind(this));
+	}
+
+	toggleTheMenu() {
+		this.menuContent.toggleClass("site-header__menu-content--is-visible");
+		this.siteHeader.toggleClass("site-header--is-expanded");
+		this.menuIcon.toggleClass("site-header__menu-icon--close-x");
+	}
+}
+
+export default MobileMenu;
+
+
+//linea 11
+// cuando dices: this.togglethemenu, js cree que estas hablando
+// de this como "this menuIcon", apunta al elemento del DOM
+// lo que tu quieres es this, pero como la clase Mobile menu
+// para eso sirve el bind(this), devuelve la propiedad this a la clase
--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,7 +4,8 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
-mixins = require('postcss-mixins');
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', function() {
 	//.src indica el punto de partida
@@ -13,7 +14,7 @@ gulp.task('styles', function() {
 	//luego termina la tarea para no destruir el workflow
 	//.dest indica donde dejar los archivos que pasan por el filtro
 	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+		.pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
 		.on('error', function(errorInfo) {
 			console.log(errorInfo.toString());
 			this.emit('end');
